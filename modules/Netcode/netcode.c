832a9e47506228bd4277d0ee9f3fda8279d8c2d3
Dan Bechard <dbechrd@gmail.com>
2024-09-15 21:38:50 -0400

    Replay protection test

--- modules/Netcode/netcode.c
+++ modules/Netcode/netcode.c
@@ -157,54 +157,6 @@ static void test_address()
     }
 }
 
-
-
-void test_replay_protection()
-{
-    struct netcode_replay_protection_t replay_protection;
-
-    int i;
-    for (i = 0; i < 2; i++)
-    {
-        netcode_replay_protection_reset(&replay_protection);
-
-        check(replay_protection.most_recent_sequence == 0);
-
-        // the first time we receive packets, they should not be already received
-
-        #define MAX_SEQUENCE (NETCODE_REPLAY_PROTECTION_BUFFER_SIZE * 4)
-
-        uint64_t sequence;
-        for (sequence = 0; sequence < MAX_SEQUENCE; ++sequence)
-        {
-            check(netcode_replay_protection_already_received(&replay_protection, sequence) == 0);
-            netcode_replay_protection_advance_sequence(&replay_protection, sequence);
-        }
-
-        // old packets outside buffer should be considered already received
-
-        check(netcode_replay_protection_already_received(&replay_protection, 0) == 1);
-
-        // packets received a second time should be flagged already received
-
-        for (sequence = MAX_SEQUENCE - 10; sequence < MAX_SEQUENCE; ++sequence)
-        {
-            check(netcode_replay_protection_already_received(&replay_protection, sequence) == 1);
-        }
-
-        // jumping ahead to a much higher sequence should be considered not already received
-
-        check(netcode_replay_protection_already_received(&replay_protection, MAX_SEQUENCE + NETCODE_REPLAY_PROTECTION_BUFFER_SIZE) == 0);
-
-        // old packets should be considered already received
-
-        for (sequence = 0; sequence < MAX_SEQUENCE; ++sequence)
-        {
-            check(netcode_replay_protection_already_received(&replay_protection, sequence) == 1);
-        }
-    }
-}
-
 void test_client_create()
 {
     {
